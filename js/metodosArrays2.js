7f8ede333a37bef5fd33dbad7288119003c08e44
devcarlos21 <devcarlos21@gmail.com>
2025-03-09 20:42:27 -0600

    Por el momento termino con el repaso de array methods

--- js/metodosArrays2.js
+++ js/metodosArrays2.js
@@ -14,10 +14,28 @@ const agencia = [
 const resultadoNom = agencia.some(function(agencia){
     return agencia.nombre === "Tiida";
 });
-
 console.log(resultadoNom);
 
 
 // Búsqueda por SOME, pero con arrow function (IDEAL Y CORTA)
 const resultadoNom2 = agencia.some(auto => auto.nombre === "Nissan March");
 console.log(resultadoNom2);
+
+
+// SUMA de un arreglo de objetos
+const sumaAutos = agencia.reduce(function(total, costo){
+    return total + costo.precio;
+}, 0);
+console.log(sumaAutos);
+
+// SUMA con Arrow Function
+const sumaAutos2 = agencia.reduce((total, costo) => total + costo.precio, 0);
+console.log("Segundo Método: ", sumaAutos2);
+
+
+// FILTER
+// Podemos traer solo ese, todos menos ese, mayores o menores a él, etc.
+const filtrado = agencia.filter(function(unico){
+    return unico.precio > 380000;
+});
+console.log(filtrado);
