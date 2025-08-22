9bdc99d2d1183df39658bbc1db4defd17955e762
devcarlos21 <devcarlos21@gmail.com>
2025-08-22 12:59:43 -0600

    De nuevo, cambios en paramArgum

--- js/18 paramArgum.js
+++ js/18 paramArgum.js
@@ -1,8 +1,11 @@
 
-function suma(num1, num2){ // Parámetros
+function suma(num1 = 0, num2 = 0){ // Parámetros por default
     console.log(num1 + num2);
 }
 suma(10,5); // Argumentos
+suma(3,7);
+suma(3,7);
+suma(3);
 
 
 const saludo = function(frase1, nombre){
