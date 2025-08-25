578c9ed8e833fdde684a1de1979240f1e8aa1019
devcarlos21 <devcarlos21@gmail.com>
2025-08-25 11:53:50 -0600

    Repaso rápido de if and elseif

--- js/22 estructurasControl.js
+++ js/22 estructurasControl.js
@@ -1,2 +1,22 @@
 // Estructuras Control
 
+// if 
+const rol = "Admin";
+
+if(rol === "Admin"){
+    console.log('Bienvenido, permisos totales');
+} else{
+    console.log('Acceso denegado');
+}
+
+
+// else if
+const cargo = "Editor";
+if(cargo === "Admin"){
+    console.log('Bienvenido, permisos totales');
+} else if(cargo === "Editor"){
+    console.log('Bienvenido, permisos de EDITOR');
+}
+else{
+    console.log('Acceso denegado');
+}
